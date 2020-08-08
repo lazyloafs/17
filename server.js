4d46c79c8e232101538d55e8dc40c2a9262ef6cd
lazyloafs <60455346+lazyloafs@users.noreply.github.com>
2020-08-08 12:04:24 -0400

    :rocket:

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
+// Dependencies
 const express = require("express");
-const logger = require("morgan");
+const morgan = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = 3000;
-
+// Setting up Express App
 const app = express();
+const PORT = process.env.PORT || 8000;
 
-app.use(logger("dev"));
+app.use(morgan("dev"));
 
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
-app.use(express.static("public"));
+// db mongo
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false
+})
 
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// Creating Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-// routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/view.js"));
-
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+// Starts the server to begin listening
+app.listen(PORT, function(){
+    console.log(`App listening on Port ${PORT}!`);
 });
